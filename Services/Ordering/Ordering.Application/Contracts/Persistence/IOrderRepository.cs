0767ad63817e3bc693009412288cc14be3d260ce
PaydarNymaPC <nima.jalalabadii@gmail.com>
2023-08-07 00:40:32 +0430

    update order repository

--- Services/Ordering/Ordering.Application/Contracts/Persistence/IOrderRepository.cs
+++ Services/Ordering/Ordering.Application/Contracts/Persistence/IOrderRepository.cs
@@ -9,6 +9,6 @@ namespace Ordering.Application.Contracts.Persistence
 {
     public interface IOrderRepository: IAsyncRepository<Order>
     {
-        Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+        Task<IEnumerable<Order>> GetOrdersByUserName(string userName );
     }
 }
