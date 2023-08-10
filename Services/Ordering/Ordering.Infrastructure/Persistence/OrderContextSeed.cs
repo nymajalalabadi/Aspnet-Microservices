456fb65e7e1e5ae3ab79fdb7a7cb5fb809a80781
PaydarNymaPC <nima.jalalabadii@gmail.com>
2023-08-10 19:11:10 +0430

    Seed Data

--- Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
+++ Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -13,10 +13,11 @@ namespace Ordering.Infrastructure.Persistence
     {
         public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
         {
-            if (await orderContext.Orders.AnyAsync())
+            if (!await orderContext.Orders.AnyAsync())
             {
                 await orderContext.Orders.AddRangeAsync(GetPreconfiguredOrders());
                 await orderContext.SaveChangesAsync();
+                logger.LogInformation("data seed section configured");
             }
         }
 
@@ -24,7 +25,7 @@ namespace Ordering.Infrastructure.Persistence
         {
             return new List<Order>()
             {
-                new Order()
+                new Order
                 {
                     FirstName = "nyma",
                     LastName = "jalali",
@@ -32,7 +33,17 @@ namespace Ordering.Infrastructure.Persistence
                     EmailAddress = "email@email.com",
                     City = "mahabad",
                     Country = "iran",
-                    TotalPrice = 1000000,
+                    TotalPrice = 1000000
+                },
+                new Order
+                {
+                    FirstName = "mohammad",
+                    LastName = "ordookhani",
+                    UserName = "mohammad",
+                    EmailAddress = "test@test.com",
+                    City = "tehran",
+                    Country = "iran",
+                    TotalPrice = 10000
                 }
             };
         }
--- Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
+++ Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -2,10 +2,8 @@
 using Ordering.Application.Contracts.Persistence;
 using Ordering.Domain.Entities;
 using Ordering.Infrastructure.Persistence;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Ordering.Infrastructure.Repositories
@@ -13,7 +11,9 @@ namespace Ordering.Infrastructure.Repositories
     public class OrderRepository : RepositoryBase<Order>, IOrderRepository
     {
         public OrderRepository(OrderContext dbContext) : base(dbContext)
-        {}
+        {
+
+        }
 
         public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
         {
