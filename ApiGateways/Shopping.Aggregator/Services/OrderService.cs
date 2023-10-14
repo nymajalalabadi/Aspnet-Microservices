21f3db04185fb0db8cdb3794ae5f64139c338c29
PaydarNymaPC <nima.jalalabadii@gmail.com>
2023-10-14 11:03:36 +0330

    Complete OrderService

--- ApiGateways/Shopping.Aggregator/Services/OrderService.cs
+++ ApiGateways/Shopping.Aggregator/Services/OrderService.cs
@@ -1,14 +1,25 @@
-﻿using Shopping.Aggregator.Model;
+﻿using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Model;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Shopping.Aggregator.Services
 {
     public class OrderService : IOrderService
     {
-        public Task<IEnumerable<OrderResponseModel>> GetOrderByUserName(string userName)
+        private readonly HttpClient _client;
+
+        public OrderService(HttpClient client)
         {
-            throw new System.NotImplementedException();
+            _client = client;
+        }
+
+        public async Task<IEnumerable<OrderResponseModel>> GetOrderByUserName(string userName)
+        {
+            var response = await _client.GetAsync($"/api/v1/Order/{userName}");
+
+            return await response.ReadContentAs<List<OrderResponseModel>>();
         }
     }
 }
